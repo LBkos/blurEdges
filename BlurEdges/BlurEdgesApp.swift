6d631c4295041a035df32a3ad54dbc1dc269f476
LBkos <constantin.l.o.p@gmail.com>
2023-07-11 16:33:12 +0700

    Initial Commit

---
+++ BlurEdges/BlurEdgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurEdgesApp.swift
+//  BlurEdges
+//
+//  Created by Константин Лопаткин on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BlurEdgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
